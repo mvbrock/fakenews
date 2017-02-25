252a4bad01c36a8525395d4d07822003ed3d856f
Matt Brock <matt@grouptrendanalytics.com>
2017-02-24 21:04:42 -0600

    Added ability to save model and CSV output of predictions

--- model-builder.py
+++ model-builder.py
@@ -4,6 +4,7 @@ from pyspark.ml import Pipeline
 from pyspark.ml.feature import Tokenizer, StringIndexer, StopWordsRemover, NGram, HashingTF, IndexToString
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+import pyspark.sql.functions
 import argparse
 import numpy
 
@@ -16,6 +17,11 @@ parser.add_argument('--data-partitions', type=int, default=1,
     help='The number of partitions to split the initial set into for better parallelization')
 parser.add_argument('--tf-features', type=int, default=10000,
     help='The number of TF features to include in the classifier')
+parser.add_argument('--output-model', type=str, required=True,
+    help='The path of the output classifier model')
+parser.add_argument('--output-predictions', type=str, required=True,
+    help='The path of the output predictions in CSV format')
+
 args = parser.parse_args()
 
 # Create the Spark and Spark SQL contexts
@@ -62,11 +68,25 @@ unindexer = IndexToString(inputCol = 'prediction', outputCol='prediction_label',
 # Create the pipeline to run each of the stages
 pipeline = Pipeline(stages = [tokenizer, indexer, stopwords, ngram, hashtf, randforest, unindexer])
 
+# Fit the model
 model = pipeline.fit(training_data)
+
+# Apply test data to get predictions
 predictions = model.transform(test_data)
 
-print(predictions.select('prediction_label', 'label').show())
+# Write predictions out to a CSV file
+csv_predictions = predictions.select('prediction_label', 'label', 'probability')
 
+def map_predictions(pred_rdd):
+    return (pred_rdd[0], pred_rdd[1], pred_rdd[2][0].item(), pred_rdd[2][1].item())
+exploded_predictions = csv_predictions.rdd.map(map_predictions).repartition(1)
+exploded_predictions_df = scSql.createDataFrame(exploded_predictions, ['prediction_label', 'label', 'prob_real', 'prob_fake'])
+exploded_predictions_df.write.csv(args.output_predictions)
+
+# Evaluate the accuracy of the predictions
 evaluator = MulticlassClassificationEvaluator(labelCol="label_index", predictionCol="prediction", metricName="accuracy")
 accuracy = evaluator.evaluate(predictions)
-print("Test Error = %g" % (1.0 - accuracy))
+print("test error: %g" % (1.0 - accuracy))
+
+# Save the model to a file
+model.save(args.output_model)
